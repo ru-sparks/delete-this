971dd201cbb06c6ac74ef2d45300e485b54ccff7
James Sparks <ru_sparks@yahoo.com>
2026-09-10 16:03:27 -0700

    fix Macys cash register

---
+++ src/main/java/edu/gcu/cst239/student/bill/lab0/App.java
@@ -0,0 +1,10 @@
+package edu.gcu.cst239.student.bill.lab0;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Use your roster name exactly as it appears in the course roster.
+        System.out.println("Roster name: First Last");
+        System.out.println("Hello, World!");
+    }
+}
